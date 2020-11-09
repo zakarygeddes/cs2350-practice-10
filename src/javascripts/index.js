337b17f99c195fab831ceba538f96f143799f46c
zakarygeddes <zakarygeddes@weber.edu>
2020-11-09 02:51:49 -0700

    Final Commit

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -8,3 +8,47 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 //TODO
+import { movies } from './movies'
+
+let featured_movie = document.querySelector('.featured')
+for(let m of movies){
+    let movie_thumb = document.getElementById('m' + m.id)
+    movie_thumb.innerHTML = `
+        <img src="${m.poster}">
+    `
+
+    movie_thumb.onclick = function(){
+        selectMovie(m)
+    }
+}
+
+function selectMovie(m){
+    featured_movie.innerHTML = `
+    <img src="${m.poster}" style="float: left;">
+    <h1>${m.title}</h1>
+    <p>${m.plot}</p>
+    `
+}
+
+function searchMovies(event){
+    if(event){
+        event.preventDefault()
+    }
+
+    let input = document.querySelector('[type="search"]').value || ""
+    for(let m of movies){
+        let movie_thumb = document.getElementById('m' + m.id)
+        if(m.title.toUpperCase().indexOf(input.toUpperCase()) == -1){
+            // movie_thumb.classList.add('hidden')
+            movie_thumb.style.display = 'none'
+        } else {
+            // movie_thumb.classList.remove('hidden')
+            movie_thumb.style.display = 'block'
+        }
+    }
+
+}
+
+document.querySelector('button').onclick = searchMovies
+document.querySelector('[type="search"]').onsearch = searchMovies
+document.forms[0].addEventListener('submit', searchMovies, false)
