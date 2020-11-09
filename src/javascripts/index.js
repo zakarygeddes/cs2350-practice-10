ac9aa1eec2bc3667dbfc5d3a570322e4b8b6fb21
zakarygeddes <70238967+zakarygeddes@users.noreply.github.com>
2020-11-09 01:40:23 -0700

    Initial commit

---
+++ src/javascripts/index.js
@@ -0,0 +1,10 @@
+// Required by Webpack - do not touch
+require.context('../', true, /\.(html|json|txt|dat)$/i)
+require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
+require.context('../stylesheets/', true, /\.(css|scss)$/i)
+
+// JavaScript
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+//TODO
